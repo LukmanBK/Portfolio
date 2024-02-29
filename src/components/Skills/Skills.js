45bf9eae943cc623a522022436b453aeec9ad9bb
LukmanBK <lukmanbakari@gmail.com>
2024-02-29 14:07:16 +0100

    integration section competences

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import dataSkills from '../../data/skills.json';
+import { motion } from 'framer-motion';
+import './skills.css'; 
+
+const Skills = () => {
+  const animationProject = {
+    initial: {
+      opacity: 0,
+      y: 70,
+    },
+    animate: (id) => ({
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.3,
+        delay: 0.15 * id,
+      },
+    }),
+  };
+
+  return (
+    <section id="skills" className="skills-section">
+      <h2 className="skills-title">Mes compétences</h2>
+      <div className="skills-container">
+        {dataSkills.map((data, id) => (
+          <motion.div
+            key={id}
+            className="skill-card"
+            variants={animationProject}
+            initial="initial"
+            whileInView="animate"
+            custom={id}
+            viewport={{ once: true }}
+          >
+            <div className="skill-image-container">
+              <img
+                src={data.img}
+                alt={`icône de ${data.title}`}
+                className="skill-image"
+              />
+              <div className="skill-description" title={data.title}>
+                <p className="skill-text">{data.name}</p>
+              </div>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,32 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Footer from '../components/Footer/Footer'
-import About from '../components/About/About'
-import Intro from '../components/Intro/Intro'
-const Home= () => {
-    return(
-        <>
-        <header>
-        <Navbar />
-        </header>
-        <main>
-        <section id='accueil'>
-            <Intro />
-        </section>
-        <section id='about'>
-            <About />
-        </section>
-        </main>
-        <footer id='contact'>
-            <Footer />
-        </footer>
-        </>
-    )
-}
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import About from "../components/About/About";
+import Intro from "../components/Intro/Intro";
+import Skills from "../components/Skills/Skills";
 
-export default Home
+const Home = () => {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <section id="accueil">
+          <Intro />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+        <section id="skills">
+          <Skills />
+        </section>
+      </main>
+      <footer id="contact">
+        <Footer />
+      </footer>
+    </>
+  );
+};
+
+export default Home;
